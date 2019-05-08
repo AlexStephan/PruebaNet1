2c1c8f08385ca84e344720a0e82fd1c34d1b680f
AlexStephan <amoldovan@itba.edu.ar>
2019-05-08 11:24:14 -0300

    First commit! Hell yeah!!

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: tefi
+ *
+ * Created on May 8, 2019, 11:20 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("HeyHeyHEY!!!\n");
+    return (EXIT_SUCCESS);
+}
+
